0bc0b61a6286f810290bbe9e6196ec1c77e96ef7
Naka Freitag <spamnaka@gmail.com>
2024-03-23 00:12:16 -0400

    Fixed a bug where users couldn't never upload

--- src/routes/[user]/upload/+page.svelte
+++ src/routes/[user]/upload/+page.svelte
@@ -6,28 +6,6 @@
     $: user = data.user ?? "unknown individual that shouldn't be here";
     let images = [];
     const MAX_IMAGES_ALLOWED = 40;
-    switch (user) {
-        case "Naka": 
-            images = import.meta.glob('../../../lib/vtubers/Naka/*.(jpg|jpeg|svg|png)', {eager: true})
-            break;
-        case "Zuzu": 
-            images = import.meta.glob('../../../lib/vtubers/Zuzu/*.(jpg|jpeg|svg|png)', {eager: true})
-            break;
-        case "Kuro":
-            images = import.meta.glob('../../../lib/vtubers/Kuro/*.(jpg|jpeg|svg|png)', {eager: true})
-            break;
-        case "Ghost": 
-            images = import.meta.glob('../../../lib/vtubers/Ghost/*.(jpg|jpeg|svg|png)', {eager: true})
-            break;
-        default:
-            for (let i = 0; i < MAX_IMAGES_ALLOWED+1; i++)
-            {
-                images.push(i);
-            }
-            break; 
-    }
-
-    $: tooManyImages = images.length > MAX_IMAGES_ALLOWED;
 
     let groupName = '';
     let blinks = false;
@@ -160,117 +138,118 @@
     }
 </script>
 
-{#if tooManyImages}
-<div
-    id="upload-container"
-    class="h-full w-full flex flex-col align-center justify-center items-center" 
->
-    <div id="upload-box" class="bg-[#99CCFF] flex flex-col px-7 py-4 text-center">
-        <h1>{user} has too many images</h1>
-    </div>
-</div>
-{:else}
-<form
-    id="upload-container"
-    class="h-full w-full flex flex-col align-center justify-center items-center" 
-    method="POST"
-    enctype="multipart/form-data"
-
-    action="?/upload"
-    use:enhance
-    on:dragover={(e) => { e.preventDefault(); }} 
-    on:drop={dropImage}
->
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div id="upload-box" class="bg-[#99CCFF] flex flex-col px-7 py-4 text-center">
-        <h1 class="text-3xl font-bold underline mb-1">Upload</h1>
-        <label for="group-name" class="mb-1">One word to describe this version of {user}:</label>
-        <input id="group-name" type="text" name="groupName" placeholder={getRandomEmotion()} bind:value={groupName} on:input={changeGroupName} pattern="[a-zA-Z]*" required/>
-        <label class="inline-flex items-center cursor-pointer p-2">
-            <input type="checkbox" class="sr-only peer" name="blinks" bind:checked={blinks}>
-            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span class="mx-3">Does this {user} blink? {blinks ? "Yes" : "No"}</span>
-        </label>
-        <div>
-            <div class="flex justify-between">
-                <div class="w-1/2" on:dragover={(e)=>{e.preventDefault()}} id="quiet-container">
-                    <label for="file-upload-quiet" class="whitespace-nowrap flex flex-col items-center justify-center" id="quiet-label">
-                        {#if quietFiles?.[0]}
-                            QUIET FILE
-                            <img id="quiet-image" alt={`${groupName} quiet`} width=150 height=150 src={URL.createObjectURL(quietFiles?.[0])} class="h-[unset]"/>
-                        {:else}
-                            When {user} is quiet
-                            <span class="w-full flex flex-col border-2 border-dashed rounded-xl cursor-pointer" id="quiet-prompt">
-                                <span class="text-6xl" id="quiet-plus">+</span>
-                                <span id="quiet-prompt-text">Click or Drop</span>
-                            </span>
-                        {/if}
-                    </label>
-                    <input id="file-upload-quiet" type="file" name="quietFileToUpload" class="w-[201px] bg-[#FF7400] hidden"
-                    accept="image/*" on:dragover={(e)=>{e.preventDefault()}} bind:this={quietInputFileElement} bind:files={quietFiles} required/>
-                </div>
-                <span class="border mt-3 mx-2"/>
-                <div class="w-1/2" on:dragover={(e)=>{e.preventDefault()}} id="speak-container">
-                    <label for="file-upload-speak" class="whitespace-nowrap flex flex-col items-center justify-center" id="speak-label">
-                        {#if speakFiles?.[0]}
-                            SPEAK FILE
-                            <img id="speak-image" alt={`${groupName} speak`} width=150 height=150 src={URL.createObjectURL(speakFiles?.[0])} class="h-[unset]"/>
-                        {:else}
-                            When {user} speaks
-                            <span class="w-full flex flex-col border-2 border-dashed rounded-xl cursor-pointer" id="speak-prompt">
-                                <span class="text-6xl" id="speak-plus">+</span>
-                                <span id="speak-prompt-text">Click or Drop</span>
-                            </span>
-                        {/if}
-                    </label>
-                    <input id="file-upload-speak" type="file" name="speakFileToUpload" class="w-[201px] bg-[#FF7400] hidden"
-                    accept="image/*" on:dragover={(e)=>{e.preventDefault()}} bind:this={speakInputFileElement} bind:files={speakFiles} required/>
-                </div>
-            </div>
-            {#if blinks}
-                <div class="flex justify-between">
-                    <div class="w-1/2 border-t-2 mt-2" on:dragover={(e)=>{e.preventDefault()}} id="quiet-blink-container">
-                        <label for="file-upload-quiet-blink" class="whitespace-nowrap flex flex-col items-center justify-center" id="quiet-blink-label">
-                            {#if quietBlinkFiles?.[0]}
-                                QUIET BLINK FILE
-                                <img id="quiet-blink-image" alt={`${groupName} quiet blink`} width=150 height=150 src={URL.createObjectURL(quietBlinkFiles?.[0])} class="h-[unset]"/>
-                            {:else}
-                                When {user} is quiet and blinks
-                                <span class="w-full flex flex-col border-2 border-dashed rounded-xl cursor-pointer" id="quiet-blink-prompt">
-                                    <span class="text-6xl" id="quiet-blink-plus">+</span>
-                                    <span id="quiet-blink-prompt-text">Click or Drop</span>
-                                </span>
-                            {/if}
-                        </label>
-                        <input id="file-upload-quiet-blink" type="file" name="quietBlinkFileToUpload" class="w-[201px] bg-[#FF7400] hidden"
-                        accept="image/*" on:dragover={(e)=>{e.preventDefault()}} bind:this={quietBlinkInputFileElement} bind:files={quietBlinkFiles} required/>
-                    </div>
-                    <span class="border mt-3 mx-2"/>
-                    <div class="w-1/2 border-t-2 mt-2" on:dragover={(e)=>{e.preventDefault()}} id="speak-blink-container">
-                        <label for="file-upload-speak-blink" class="whitespace-nowrap flex flex-col items-center justify-center" id="speak-blink-label">
-                            {#if speakBlinkFiles?.[0]}
-                                SPEAK BLINK FILE
-                                <img id="speak-blink-image" alt={`${groupName} speaking blink`} width=150 height=150 src={URL.createObjectURL(speakBlinkFiles?.[0])} class="h-[unset]"/>
-                            {:else}
-                                When {user} speaks and blinks
-                                <span class="w-full flex flex-col border-2 border-dashed rounded-xl cursor-pointer" id="speak-blink-prompt">
-                                    <span class="text-6xl" id="speak-blink-plus">+</span>
-                                    <span id="speak-blink-prompt-text">Click or Drop</span>
-                                </span>
-                            {/if}
-                        </label>
-                        <input id="file-upload-speak-blink" type="file" name="speakBlinkFileToUpload" class="w-[201px] bg-[#FF7400] hidden"
-                        accept="image/*" on:dragover={(e)=>{e.preventDefault()}} bind:this={speakBlinkInputFileElement} bind:files={speakBlinkFiles} required/>
-                    </div>
-                </div> 
-            {/if}
+<!-- TODO: Add a check to see if the user has too many images.  If they do, stop them from uploading. -->
+{#if false}
+    <div
+        id="upload-container"
+        class="h-full w-full flex flex-col align-center justify-center items-center" 
+    >
+        <div id="upload-box" class="bg-[#99CCFF] flex flex-col px-7 py-4 text-center">
+            <h1 class="text-2xl underline mb-1"><strong>{user}</strong> has too many images</h1>
         </div>
     </div>
-    {#if readyForAction }
-        <button type="submit">Submit</button>
-    {/if}
-</form>
+{:else}
+    <form
+        id="upload-container"
+        class="h-full w-full flex flex-col align-center justify-center items-center" 
+        method="POST"
+        enctype="multipart/form-data"
+
+        action="?/upload"
+        use:enhance
+        on:dragover={(e) => { e.preventDefault(); }} 
+        on:drop={dropImage}
+    >
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div id="upload-box" class="bg-[#99CCFF] flex flex-col px-7 py-4 text-center">
+            <h1 class="text-3xl font-bold underline mb-1">Upload</h1>
+            <label for="group-name" class="mb-1">One word to describe this version of {user}:</label>
+            <input id="group-name" type="text" name="groupName" placeholder={getRandomEmotion()} bind:value={groupName} on:input={changeGroupName} pattern="[a-zA-Z]*" required/>
+            <label class="inline-flex items-center cursor-pointer p-2">
+                <input type="checkbox" class="sr-only peer" name="blinks" bind:checked={blinks}>
+                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span class="mx-3">Does this {user} blink? {blinks ? "Yes" : "No"}</span>
+            </label>
+            <div>
+                <div class="flex justify-between">
+                    <div class="w-1/2" on:dragover={(e)=>{e.preventDefault()}} id="quiet-container">
+                        <label for="file-upload-quiet" class="whitespace-nowrap flex flex-col items-center justify-center" id="quiet-label">
+                            {#if quietFiles?.[0]}
+                                QUIET FILE
+                                <img id="quiet-image" alt={`${groupName} quiet`} width=150 height=150 src={URL.createObjectURL(quietFiles?.[0])} class="h-[unset]"/>
+                            {:else}
+                                When {user} is quiet
+                                <span class="w-full flex flex-col border-2 border-dashed rounded-xl cursor-pointer" id="quiet-prompt">
+                                    <span class="text-6xl" id="quiet-plus">+</span>
+                                    <span id="quiet-prompt-text">Click or Drop</span>
+                                </span>
+                            {/if}
+                        </label>
+                        <input id="file-upload-quiet" type="file" name="quietFileToUpload" class="w-[201px] bg-[#FF7400] hidden"
+                        accept="image/*" on:dragover={(e)=>{e.preventDefault()}} bind:this={quietInputFileElement} bind:files={quietFiles} required/>
+                    </div>
+                    <span class="border mt-3 mx-2"/>
+                    <div class="w-1/2" on:dragover={(e)=>{e.preventDefault()}} id="speak-container">
+                        <label for="file-upload-speak" class="whitespace-nowrap flex flex-col items-center justify-center" id="speak-label">
+                            {#if speakFiles?.[0]}
+                                SPEAK FILE
+                                <img id="speak-image" alt={`${groupName} speak`} width=150 height=150 src={URL.createObjectURL(speakFiles?.[0])} class="h-[unset]"/>
+                            {:else}
+                                When {user} speaks
+                                <span class="w-full flex flex-col border-2 border-dashed rounded-xl cursor-pointer" id="speak-prompt">
+                                    <span class="text-6xl" id="speak-plus">+</span>
+                                    <span id="speak-prompt-text">Click or Drop</span>
+                                </span>
+                            {/if}
+                        </label>
+                        <input id="file-upload-speak" type="file" name="speakFileToUpload" class="w-[201px] bg-[#FF7400] hidden"
+                        accept="image/*" on:dragover={(e)=>{e.preventDefault()}} bind:this={speakInputFileElement} bind:files={speakFiles} required/>
+                    </div>
+                </div>
+                {#if blinks}
+                    <div class="flex justify-between">
+                        <div class="w-1/2 border-t-2 mt-2" on:dragover={(e)=>{e.preventDefault()}} id="quiet-blink-container">
+                            <label for="file-upload-quiet-blink" class="whitespace-nowrap flex flex-col items-center justify-center" id="quiet-blink-label">
+                                {#if quietBlinkFiles?.[0]}
+                                    QUIET BLINK FILE
+                                    <img id="quiet-blink-image" alt={`${groupName} quiet blink`} width=150 height=150 src={URL.createObjectURL(quietBlinkFiles?.[0])} class="h-[unset]"/>
+                                {:else}
+                                    When {user} is quiet and blinks
+                                    <span class="w-full flex flex-col border-2 border-dashed rounded-xl cursor-pointer" id="quiet-blink-prompt">
+                                        <span class="text-6xl" id="quiet-blink-plus">+</span>
+                                        <span id="quiet-blink-prompt-text">Click or Drop</span>
+                                    </span>
+                                {/if}
+                            </label>
+                            <input id="file-upload-quiet-blink" type="file" name="quietBlinkFileToUpload" class="w-[201px] bg-[#FF7400] hidden"
+                            accept="image/*" on:dragover={(e)=>{e.preventDefault()}} bind:this={quietBlinkInputFileElement} bind:files={quietBlinkFiles} required/>
+                        </div>
+                        <span class="border mt-3 mx-2"/>
+                        <div class="w-1/2 border-t-2 mt-2" on:dragover={(e)=>{e.preventDefault()}} id="speak-blink-container">
+                            <label for="file-upload-speak-blink" class="whitespace-nowrap flex flex-col items-center justify-center" id="speak-blink-label">
+                                {#if speakBlinkFiles?.[0]}
+                                    SPEAK BLINK FILE
+                                    <img id="speak-blink-image" alt={`${groupName} speaking blink`} width=150 height=150 src={URL.createObjectURL(speakBlinkFiles?.[0])} class="h-[unset]"/>
+                                {:else}
+                                    When {user} speaks and blinks
+                                    <span class="w-full flex flex-col border-2 border-dashed rounded-xl cursor-pointer" id="speak-blink-prompt">
+                                        <span class="text-6xl" id="speak-blink-plus">+</span>
+                                        <span id="speak-blink-prompt-text">Click or Drop</span>
+                                    </span>
+                                {/if}
+                            </label>
+                            <input id="file-upload-speak-blink" type="file" name="speakBlinkFileToUpload" class="w-[201px] bg-[#FF7400] hidden"
+                            accept="image/*" on:dragover={(e)=>{e.preventDefault()}} bind:this={speakBlinkInputFileElement} bind:files={speakBlinkFiles} required/>
+                        </div>
+                    </div> 
+                {/if}
+            </div>
+        </div>
+        {#if readyForAction }
+            <button type="submit">Submit</button>
+        {/if}
+    </form>
 {/if}
 <style>
     button{
